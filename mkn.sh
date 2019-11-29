12328b23714a01623da20f142714bfb7bede8f5c
philip.deegan <philip.deegan@gmail.com>
2019-11-29 16:17:07 +0100

    more

--- mkn.sh
+++ mkn.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 set -e
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-VERSION="master"
 DIR="samrai"
-GIT_URL="https://github.com/llnl/samrai"
-HAS=1
-FFF=("include" "lib" "samrai" "share")
+GIT_URL="https://github.com/llnl/$DIR"
+VERSION="master"
+FFF=("include" "lib" "$DIR" "share")
 [ ! -z "$MKN_CLEAN" ] && (( $MKN_CLEAN == 1 )) && for f in ${FFF[@]}; do rm -rf $CWD/$f; done
-[ ! -d "$CWD/$DIR" ] && HAS=0 && git clone --depth 1 $GIT_URL -b $VERSION $DIR --recursive
+[ ! -d "$CWD/$DIR" ] && git clone --depth 1 $GIT_URL -b $VERSION $DIR --recursive
 cd $CWD/$DIR
-[ $HAS -eq 1 ] && git pull --recurse-submodules origin $VERSION
 rm -rf build && mkdir build && cd build
 read -r -d '' CMAKE <<- EOM || echo "running cmake"
 cmake -DCMAKE_INSTALL_PREFIX=$CWD
